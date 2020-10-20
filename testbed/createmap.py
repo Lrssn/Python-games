3c233191e04a671478b74923b26d8b6c1e25a720
Erik <lrssn95@gmail.com>
2020-10-20 11:25:41 +0200

    improved map creator

--- testbed/createmap.py
+++ testbed/createmap.py
@@ -8,7 +8,7 @@ def createmap(sizex, sizey, scale, octaves, persistence, lacunarity):
     water = [65,105,225]
     grass = [34,139,34]
     forest = [0, 255, 12]
-    farmland = [245, 222, 179]
+    farmland = [139,69,19]
     beach = [238, 214, 175]
     for i in range(sizey):
         for j in range(sizex):
@@ -17,19 +17,20 @@ def createmap(sizex, sizey, scale, octaves, persistence, lacunarity):
                                     octaves=octaves, 
                                     persistence=persistence, 
                                     lacunarity=lacunarity, 
-                                    repeatx=1024, 
-                                    repeaty=1024, 
-                                    base=0)
+                                    repeatx=2048, 
+                                    repeaty=2048, 
+                                    base=1)
     set_image = numpy.zeros(img.shape)
     for i in range(sizey):
         for j in range(sizex):
-            if img[i][j] < -0.05:
+            if img[i][j] < -0.02:
                 set_image[i][j] = 0
             elif img[i][j] < 0:
                 set_image[i][j] = 1
             elif img[i][j] < 1.0:
                 set_image[i][j] = 2
-    flood_img = cellular_automata(sizex, sizey)
+    forest_img = cellular_automata(sizex, sizey)
+    farmland_img = cellular_automata(sizex, sizey, 0.7, 6, 4, 6, 3)
     color_image = numpy.zeros(img.shape+(3,))
     for i in range(sizey):
         for j in range(sizex):
@@ -38,51 +39,53 @@ def createmap(sizex, sizey, scale, octaves, persistence, lacunarity):
             elif set_image[i][j] == 1:
                 color_image[i][j] = beach
             elif set_image[i][j] == 2:
-                if flood_img[i][j] == 0:
+                if forest_img[i][j] == 0:
                     color_image[i][j] = forest
-                elif flood_img[i][j] == 1:
-                    color_image[i][j] == farmland
+                elif forest_img[i][j] == 1:
+                    if farmland_img[i][j] == 0:
+                        color_image[i][j] = farmland
+                    elif farmland_img[i][j] == 1:
+                        color_image[i][j] = grass
 
     imageio.imwrite('noise.png', color_image)
 
-def cellular_automata(sizex, sizey):
+def cellular_automata(sizex, sizey, live_chance = 0.4, nrofsimsteps = 7, starvationLimit = 4, overpopLimit = 6, birthNumber = 4):
     shape = (sizey, sizex)
-    Forest = 0
-    Farm = 1
-    forestsize = 0.4
+    dead = 0
+    live = 1
+    
 
     new_map = numpy.ones(shape)
     for i in range(shape[0]):
         for j in range(shape[1]):
             choice = random.uniform(0, 1)
-            new_map[i][j] = Forest if choice < forestsize else Farm
+            new_map[i][j] = dead if choice < live_chance else live
 
-    # run for 6 generations
-    nrofsimsteps = 7
-    starvationLimit = 4
-    overpopLimit = 6
-    birthNumber = 4
     temp_map = numpy.ones(shape)
+
     for simstep in range(nrofsimsteps):
         for i in range(shape[0]):
             for j in range(shape[1]):
                 farmtiles = nr_of_farmtiles(new_map, j, i, shape)
-                if new_map[i][j] == Farm:
+                if new_map[i][j] == live:
                     if farmtiles < starvationLimit:
-                        temp_map[i][j] = Forest
+                        temp_map[i][j] = dead
                     if farmtiles > overpopLimit:
-                        temp_map[i][j] = Forest
+                        temp_map[i][j] = dead
                 else:
                     if farmtiles == birthNumber:
-                        temp_map[i][j] = Farm
+                        temp_map[i][j] = live
 
         new_map = temp_map
-
-    colored = numpy.ones(shape+(3,))
+    noiseimg = numpy.ones(shape+(3,))
     for i in range(shape[0]):
         for j in range(shape[1]):
-            colored[i][j] = [0, 255, 12] if new_map[i][j] == Forest else [245, 222, 179]
-    imageio.imwrite('colored.png', colored)
+            if new_map[i][j] == 0:
+                noiseimg[i][j] = [245, 222, 179]
+            else:
+                noiseimg[i][j] = [34,139,34]
+    
+    imageio.imwrite('noise1.png', noiseimg)
     return new_map
 
 def nr_of_farmtiles(old_map, x, y, shape):
